c795ed5201619514a3ae6c22d93067a62652df7a
ceciliazaragoza <czarago3@lion.lmu.edu>
2024-03-23 12:45:42 -0700

    video works, no controls though

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { SignIn, SignOut, useAuthentication } from "./services/authService";
-import Timer from "./timer";
+// import logo from "./logo.svg";
+// import "./App.css";
+import { SignIn, SignOut, useAuthentication } from "./services/authService.js";
+import VideoCall from "./components/VideoCall.js";
+// import Timer from "./timer";
 
 function App() {
-  const user = useAuthentication()
+  const user = useAuthentication();
 
   return (
     <div className="App">
       <header>
-        <span>pomodoro app</span> 
+        <span>pomodoro app</span>
+        <VideoCall />
         {!user ? <SignIn /> : <SignOut />}
       </header>
     </div>
--- src/components/timer.js
+++ src/components/timer.js
@@ -1,22 +1,22 @@
-import React, { useState, useEffect } from "react";
+// import React, { useState, useEffect } from "react";
 
-function Timer() {
-  const [time, setTime] = useState(new Date());
+// function () {
+//   const [time, setTime] = useState(new Date());
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setTime(new Date());
-    }, 1000);
+//   useEffect(() => {
+//     const interval = setInterval(() => {
+//       setTime(new Date());
+//     }, 1000);
 
-    return () => clearInterval(interval);
-  }, []);
+//     return () => clearInterval(interval);
+//   }, []);
 
-  return (
-    <div>
-      <h1>Current Time:</h1>
-      <p>{time.toLocaleTimeString()}</p>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <h1>Current Time:</h1>
+//       <p>{time.toLocaleTimeString()}</p>
+//     </div>
+//   );
+// }
 
-export default Timer;
+// export default Timer;
