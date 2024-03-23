123e5a3b6b50684154a418354b5fd0482f8cff01
Nicholas Lee <nicholas.lee200311@gmail.com>
2024-03-23 13:24:48 -0700

    Solve merge conflict

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import "./App.css";
-<<<<<<< HEAD
 import { SignIn, SignOut, useAuthentication } from "./services/authService";
 import TaskList from  "./components/TaskList";
-=======
-import { SignIn, SignOut, useAuthentication } from "./services/authService.js";
-import React, { useEffect, useMemo, useRef, useState } from "react";
 // import Timer from "timer.js";
->>>>>>> raihana
 
 function App() {
   const user = useAuthentication();
