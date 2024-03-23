929640342f2aa6a5de311c247ac17dbafd745317
raihanaza <86710982+raihanaza@users.noreply.github.com>
2024-03-23 16:31:54 -0700

    adjusting the clock so that it transitions to top left after certain border

--- src/App.js
+++ src/App.js
@@ -5,56 +5,67 @@ import Timer from './components/timer'
 import AnalogClock from './components/AnalogClock'
 import VideoCall from './components/VideoCall.js'
 import './components/clock.css'
-import { createTask, fetchTasks, deleteTask } from './services/taskService';
-import { SignIn, SignOut, useAuthentication } from "./services/authService";
-import TaskList from './components/TaskList';
-
+import { createTask, fetchTasks, deleteTask } from './services/taskService'
+import { SignIn, SignOut, useAuthentication } from './services/authService'
+import TaskList from './components/TaskList'
 
 function App() {
-  const [tasks, setTasks] = useState([]);
-  const [taskInput, setTaskInput] = useState('');
-  const user = useAuthentication();
+  const [tasks, setTasks] = useState([])
+  const [taskInput, setTaskInput] = useState('')
+  const user = useAuthentication()
   const initial = user?.email?.[0].toUpperCase()
   const toggleSignOut = () => setShowSignOut(!showSignOut)
   const [showSignOut, setShowSignOut] = useState(false)
+  const [isClockMinimized, setIsClockMinimized] = useState(false)
 
   useEffect(() => {
     if (user) {
-      fetchUserTasks();
+      fetchUserTasks()
     }
-  }, [user]);
+  }, [user])
+
+  useEffect(() => {
+    const handleScroll = () => {
+      // Determine the condition to minimize the clock
+      const shouldBeMinimized = window.scrollY > 50
+      setIsClockMinimized(shouldBeMinimized)
+    }
+
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
 
   const fetchUserTasks = async () => {
-    console.log("Performing fetching user task")
-    console.log("Current user id is ", user.uid)
-    const userTasks = await fetchTasks(user.uid);
-    console.log("userTask is ", userTasks)
-    setTasks(userTasks);
-  };
+    console.log('Performing fetching user task')
+    console.log('Current user id is ', user.uid)
+    const userTasks = await fetchTasks(user.uid)
+    console.log('userTask is ', userTasks)
+    setTasks(userTasks)
+  }
 
   const handleAddTask = async () => {
-    if (!taskInput.trim()) return;
-    await createTask({ task: taskInput, author: user.uid, completed: false });
-    setTaskInput('');
-    console.log("Should perform fetching User Task")
-    fetchUserTasks();
-  };
+    if (!taskInput.trim()) return
+    await createTask({ task: taskInput, author: user.uid, completed: false })
+    setTaskInput('')
+    console.log('Should perform fetching User Task')
+    fetchUserTasks()
+  }
 
-  const handleDeleteTask = async (taskId) => {
-    await deleteTask(taskId);
-    fetchUserTasks();
-  };
+  const handleDeleteTask = async taskId => {
+    await deleteTask(taskId)
+    fetchUserTasks()
+  }
 
   const handleUpdateTask = async (taskId, newTaskDetail) => {
     // Update the UI state with the new task detail
     const updatedTasks = tasks.map(task => {
       if (task.id === taskId) {
-        return { ...task, task: newTaskDetail };
+        return { ...task, task: newTaskDetail }
       }
-      return task;
-    });
-    setTasks(updatedTasks);
-  };
+      return task
+    })
+    setTasks(updatedTasks)
+  }
 
   return (
     <div className="App">
@@ -74,27 +85,32 @@ function App() {
           )}
         </div>
       </header>
-      <div className="clock-container">
-        <AnalogClock /> {/* AnalogClock component */}
+      <div className={`clock-container ${isClockMinimized ? 'minimized-clock' : ''}`}>
+        <AnalogClock /> 
         <VideoCall />
       </div>
       <div>
         {!user ? (
           <h2> Login to view your tasks</h2>
-        ): (
+        ) : (
           <div>
-            <form onSubmit={(e) => { e.preventDefault(); handleAddTask(); } }>
+            <form
+              onSubmit={e => {
+                e.preventDefault()
+                handleAddTask()
+              }}
+            >
               <input
                 type="text"
                 value={taskInput}
-                onChange={(e) => setTaskInput(e.target.value)}
-                placeholder="Add a new task" />
+                onChange={e => setTaskInput(e.target.value)}
+                placeholder="Add a new task"
+              />
               <button type="submit">Add Task</button>
             </form>
-            <TaskList tasks={tasks} onDelete={handleDeleteTask} onUpdateTask={handleUpdateTask}/>
+            <TaskList tasks={tasks} onDelete={handleDeleteTask} onUpdateTask={handleUpdateTask} />
           </div>
         )}
-
       </div>
     </div>
   )
