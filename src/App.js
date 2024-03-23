6a05f0a7879c10c0216162c8f320a8f7c6e3807b
raihanaza <86710982+raihanaza@users.noreply.github.com>
2024-03-23 14:24:07 -0700

    changed the sign in look

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { SignIn, SignOut, useAuthentication } from "./services/authService.js";
-import React, { useEffect, useMemo, useRef, useState } from "react";
+import logo from './logo.svg'
+import './App.css'
+import { SignIn, SignOut, useAuthentication } from './services/authService.js'
+import React, { useEffect, useMemo, useRef, useState } from 'react'
 // import Timer from "timer.js";
 
 function App() {
-  const user = useAuthentication();
+  const user = useAuthentication()
+  const initial = user?.email?.[0].toUpperCase()
+  const toggleSignOut = () => setShowSignOut(!showSignOut)
+  const [showSignOut, setShowSignOut] = useState(false)
 
   return (
     <div className="App">
-      <header>
-        <span>pomodoro app</span>
-        {!user ? <SignIn /> : <SignOut />}
+      <header className="App-header">
+        <div className="auth-container">
+          {!user ? (
+            <SignIn />
+          ) : (
+            <>
+              <div className="greeting">Hello, {user.displayName || 'User'}</div>
+              <div className="avatar" onClick={toggleSignOut} style={{ cursor: 'pointer' }}>
+                {initial}
+              </div>
+              {showSignOut && <SignOut />}
+            </>
+          )}
+        </div>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
