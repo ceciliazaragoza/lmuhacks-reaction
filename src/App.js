e2047319f56a1797f6e4e0a57d519b6241bcbe51
raihanaza <86710982+raihanaza@users.noreply.github.com>
2024-03-23 12:43:35 -0700

    google authentication fully works, adding firebase firestore service

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { SignIn, SignOut, useAuthentication } from "../services/authService.js";
+import { SignIn, SignOut, useAuthentication } from "./services/authService.js";
 import React, { useEffect, useMemo, useRef, useState } from "react";
-import Timer from "timer.js";
+// import Timer from "timer.js";
 
 function App() {
   const user = useAuthentication();
